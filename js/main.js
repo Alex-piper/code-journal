1368bf19a80b1cba6181625169b7156053793865
Alex Piper <alexkpiper89@gmail.com>
2022-11-04 16:31:15 +0000

    Listen for 'submit' events on the journal entry form

--- js/main.js
+++ js/main.js
@@ -7,3 +7,17 @@ function getPhoto(event) {
   var $photoValue = event.target.value;
   $photoSrc.setAttribute('src', $photoValue);
 }
+
+var $form = document.querySelector('#form');
+$form.addEventListener('submit', entrySubmit);
+function entrySubmit(event) {
+  event.preventDefault();
+  var object = {};
+  object.title = $form.elements.title.value;
+  object.url = $form.elements.url.value;
+  object.notes = $form.elements.notes.value;
+  object.nextid = data.nextEntryId++;
+  data.entries.unshift(object);
+  $photoSrc.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
+}
