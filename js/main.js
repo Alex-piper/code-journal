32d3cbd7ad3ced917bc2ef6b40ae5af6c5785588
Alex Piper <alexkpiper89@gmail.com>
2022-11-17 01:04:52 +0000

    Define a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML.

--- js/main.js
+++ js/main.js
@@ -21,3 +21,45 @@ function entrySubmit(event) {
   $form.reset();
   $photoSrc.setAttribute('src', 'images/placeholder-image-square.jpg');
 }
+
+function renderEntry(entry) {
+  var li = document.createElement('li');
+  li.setAttribute('class', 'column-full entries');
+  li.setAttribute('data-entry-id', entry.nextEntryId);
+
+  var div1 = document.createElement('div');
+  div1.setAttribute('class', 'row');
+  li.appendChild(div1);
+
+  var div2 = document.createElement('div');
+  div2.setAttribute('class', 'column-half');
+  div1.appendChild(div2);
+
+  var img = document.createElement('img');
+  img.setAttribute('class', 'images');
+  img.setAttribute('src', entry.url);
+  div2.appendChild(img);
+
+  var div3 = document.createElement('div');
+  div3.setAttribute('class', 'column-half');
+  div1.appendChild(div3);
+
+  var div4 = document.createElement('div');
+  div4.setAttribute('class', 'row');
+  div3.appendChild(div4);
+
+  var div5 = document.createElement('div');
+  div5.setAttribute('class', 'column-full');
+  div4.appendChild(div5);
+
+  var $title = document.createElement('h2');
+  $title.textContent = entry.title;
+  div5.appendChild($title);
+
+  var $notes = document.createElement('p');
+  $notes.textContent = entry.notes;
+  div3.appendChild($notes);
+
+  return li;
+
+}
